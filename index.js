29ce3896e88e5ad15b53582621cfdc5b51872d1b
Abdirino <abdirinoahash@gmail.com>
2022-12-19 07:56:41 +0300

    rhino

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.remove('show')
-        }
-    })
-})
+// sliser(all slides in a container)
+const slider = document.querySelector("slider");
+// all trails
+const trail = document.querySelector(".trail").querySelector("div");
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el))
+let value = 0;
+let trailValue = 0;
+let interval = 4000;
+
+const slide = (condition) => {
+    clearInterval(start)
+    condition === "increase" ? initiateINC() : initiateDEC()
+    move(value, trailValue)
+    Animate()
+    start = setInterval(() => slide("increase"), interval)
+}
+// function for increase (forward, next) configuration
+const initiateINC = () => {
+
+}
+
+
+
+
+
+
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry);
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show')
+//         }else{
+//             entry.target.classList.remove('show')
+//         }
+//     })
+// })
+
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el))
