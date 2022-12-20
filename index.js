38e33cb777e8cd31a9797b1aef093ffb5be5d8de
Abdirino <abdirinoahash@gmail.com>
2022-12-20 15:28:36 +0300

    carousel comp n0t responsive

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
- 
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
+});
 
+// $(document).ready(function() {
+//     var autoplaySlider = $('#autoplay').lightSlider({
+//         auto:true,
+//         loop:true,
+//         pauseOnHover: true,
+//         onBeforeSlide: function (el) {
+//             $('#current').text(el.getCurrentSlideCount());
+//         } 
+//     });
+//     $('#total').text(autoplaySlider.getTotalSlideCount());
+// });
 
 // const observer = new IntersectionObserver((entries) => {
 //     entries.forEach((entry) => {
