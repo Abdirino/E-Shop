eff9664915bb4a1818b2e5bf8e4dffac2c2f7d23
Abdirino <abdirinoahash@gmail.com>
2022-12-20 10:45:04 +0300

    corousel redo

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-// sliser(all slides in a container)
-const slider = document.querySelector("slider");
-// all trails
-const trail = document.querySelector(".trail").querySelector("div");
-
-let value = 0;
-let trailValue = 0;
-let interval = 4000;
-
-const slide = (condition) => {
-    clearInterval(start)
-    condition === "increase" ? initiateINC() : initiateDEC()
-    move(value, trailValue)
-    Animate()
-    start = setInterval(() => slide("increase"), interval)
-}
-// function for increase (forward, next) configuration
-const initiateINC = () => {
-
-}
-
-
-
-
-
+ 
 
 
 // const observer = new IntersectionObserver((entries) => {
